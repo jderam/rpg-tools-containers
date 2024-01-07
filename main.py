2d864297b96d2b10d8a27bcad384e616c253c5cf
Jeremy <jderam@users.noreply.github.com>
2024-01-07 01:02:38 -0600

    Test pc (#14)
    * remove unneeded heroku file
    
    * test pc completed through secondary skill
    
    * add testpc endpoint
    
    * recompiled requirements 2023-06-06
    
    * update pip-compile command
    
    * copy test_pc.json file into container; use 4 workers instead of 2 for uvicorn
    
    * use latest debian container
    
    * add endpoint for d666 character
    
    * update pip-compile command
    
    * recompile requirements
    
    * fix d666 char endpoint
    
    * add zothique name generator

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import json
+import random
 from typing import Any, Dict, List, Optional
 
 from fastapi import FastAPI, Query
@@ -7,9 +9,14 @@ from hyperborea3.namegen import generate_name
 from hyperborea3.player_character import PlayerCharacter
 from hyperborea3.spells import get_all_spells, get_spell
 from hyperborea3.valid_data import VALID_RACES_BY_ID
-import rpg_tools.tiny_dungeon.char as td_char
+import rpg_tools.d666.char as d666_char
 import rpg_tools.gamma5.char as gamma5_char
 import rpg_tools.maze_rats.char as maze_rats_char
+from rpg_tools.misc_data.zothique_names import (
+    FEMALE_NAMES as ZOTHIQUE_FEMALE_NAMES,
+    MALE_NAMES as ZOTHIQUE_MALE_NAMES,
+)
+import rpg_tools.tiny_dungeon.char as td_char
 
 
 app = FastAPI()
@@ -62,6 +69,12 @@ async def specific_class(
     return pc.to_dict()
 
 
+@app.get("/hyperborea3/testpc")
+async def test_pc() -> Dict:
+    with open("test_pc.json", "r") as f:
+        return json.load(f)
+
+
 @app.get("/hyperborea3/spells/all")
 async def hyperborea_spells() -> List[Dict[str, Any]]:
     return get_all_spells()
@@ -110,3 +123,22 @@ async def gamma5_character():
 @app.get("/rpg-tools/maze-rats")
 async def maze_rats_character():
     return maze_rats_char.PlayerCharacter().to_dict()
+
+
+@app.get("/rpg-tools/d666")
+async def d666_character(
+    level: Optional[int] = Query(1),
+) -> Dict[str, Any]:
+    return d666_char.D666Character(level=level).to_dict()
+
+
+@app.get("/rpg-tools/zothique-names")
+async def zothique_names(
+    gender: Optional[str] = Query("any"),
+) -> str:
+    if gender.lower() == "female":
+        return random.choice(ZOTHIQUE_FEMALE_NAMES)
+    elif gender.lower() == "male":
+        return random.choice(ZOTHIQUE_MALE_NAMES)
+    else:
+        return random.choice(ZOTHIQUE_FEMALE_NAMES + ZOTHIQUE_MALE_NAMES)
